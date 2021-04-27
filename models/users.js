8f06e5e0bd15e3b2e7f31af0de5e8012604c8dae
chrisZip <kapitachristian2003@gmail.com>
2021-04-26 21:27:00 -0400

    defined schema for the users collection

---
+++ models/users.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+// Create Schema
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  LastName: {
+    type: String,
+    required: true
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  Username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+  ,
+  date: {
+    type: Date,
+    default: Date.now
+  }
+
+});
+module.exports = User = mongoose.model("users", UserSchema);
