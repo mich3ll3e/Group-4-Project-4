d57a9ad5b07e886c91d5dcde9dc8326966542e52
Michelle <michelle.ouyang@outlook.com>
2021-05-12 13:39:07 -0400

    version 1.0.0

--- server.js
+++ server.js
@@ -40,11 +40,21 @@ app.use("/api/users", users);
 app.use("/api/todo",todo);
 app.use("/api/members",members);
 
+// Serve static asset in production
+if(process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  });
+
+}
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 
 app.listen(PORT, function() {
